45ecdd661c1c6b67bdff1bf67155f9d7fcb230f0
Liam Krewer <liam.krewer@gmail.com>
2014-10-12 16:13:48 +0100

    Added Semantic-ui, updated to new express version

--- public/js/app.js
+++ public/js/app.js
@@ -8,46 +8,46 @@ app = angular.module("CompSoc", [
 app.config( function($routeProvider, $locationProvider) {
     $routeProvider.
         when('/home', {
-            templateUrl: '/partials/home.html',
+            templateUrl: '/views/partials/home.html',
             title: "Home"
         }).
         when('/blog', {
-            templateUrl: '/partials/blog.html',
+            templateUrl: '/views/partials/blog.html',
             title: "Blog"
         }).
         when('/blog/:post',{
           templateUrl: function(rp){
-            return '/partials/blog/'+rp.post+'.html'
+            return '/views/partials/blog/'+rp.post+".html"
           },
           title: "Blog"
         }).
         when('/wiki', {
-            templateUrl: '/partials/wiki.html',
+            templateUrl: '/views/partials/wiki.html',
             title: "Wiki"
         }).
         when('/wiki/:page',{
           templateUrl: function(rp){
-            return '/partials/wiki/'+rp.page+'.html'
+            return '/views/partials/wiki/'+rp.page+".html"
           },
           title: "Blog"
         }).
-        when('/users', {
-            templateUrl: '/partials/wiki.html',
-            title: "Users"
+        when('/members', {
+            templateUrl: '/views/partials/members.html',
+            title: "Members"
         }).
         when('/users/:user',{
           templateUrl: function(rp){
-            return '/partials/users/'+rp.user+'.html'
+            return '/views/partials/users/'+rp.user +".html"
           },
           title: "Users"
         }).        
         when('/hlms', {
-            templateUrl: '/partials/hlms.html',
+            templateUrl: '/views/partials/hlms.html',
             title: "HLMS"
         }).
         otherwise({ //aka default route, this will prevent 404s
-            redirectTo: '/home'
-        });
+            redirectTo: '/'
+    });
     
     
     $locationProvider.html5Mode(true); //magical function enabling going back/forward between pages using the browser buttons despite being a SPA 
@@ -60,6 +60,59 @@ app.run(function($rootScope){
 
 //The concept is to have one controller per view/page/logical unit, in this case the navigation bar/header at the top of the page will
 //be dealt with by this 'navCtrl'
-app.controller('NavCtrl' ,function($scope){ 
+app.controller('NavCtrl' ,function($scope,$timeout){ 
+    
+
+    $scope.navMenu = [
+        {
+            text: "Home",
+            url: "home",
+            active: false
+        },
+        {
+            text: "Blog",
+            url: "blog",
+            active: false
+        },
+        {
+            text: "Wiki",
+            url: "wiki",
+            active: false
+        },
+        {
+            text: "Members",
+            url: "members",
+            active: false
+        }
+    ];
+});
+app.controller('SettingsCtrl' ,function($scope,$timeout){ 
+    
+
+});
+
+
+app.controller('HomeCtrl' ,function($scope){ 
     
 });
+app.controller('WikiCtrl' ,function($scope){ 
+    
+});
+app.controller('MembersCtrl' ,function($scope){ 
+    
+});
+app.controller('BlogCtrl' ,function($scope){ 
+    
+});
+
+
+app.directive("sideBar",function($timeout){
+    return {
+        restrict: "A",
+        link: function(scope,elem,attrs){
+            console.log("sidebar initialized");
+            elem.sidebar();
+            $timeout(function(){elem.sidebar();},1000)
+        }
+    };
+})
