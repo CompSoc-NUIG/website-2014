79bd998e6fdfd913f2c5ac398af9e9dcc7625efe
Liam Krewer <liam.krewer@gmail.com>
2014-10-12 16:15:08 +0100

    added new files for previous commit

--- server.js
+++ server.js
@@ -1,73 +1,71 @@
-
+#!/bin/env node
 /*
 NodeJS server application for OpenShift using express
 */
 var express = require('express'),
-    app = express(),
-    debug = require('debug')('http'),
+    fs      = require('fs'),
     http = require('http'),
     path = require('path'),
-    router = require('./routes');
+    bodyParser = require('body-parser'),
+    compression = require('compression'),
+    serveStatic = require('serve-static'),
+    methodOverride = require('method-override'),
+    app = express(),
+    router = express.Router();
+
 
 /*
 Environment Variables
 */
 
 if (process.env.hasOwnProperty("OPENSHIFT_NODEJS_IP")) { //running on OpenShift?
-	app.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-	app.port      = process.env.OPENSHIFT_NODEJS_PORT;
-} else {
+    app.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+    app.port      = process.env.OPENSHIFT_NODEJS_PORT;
+} 
+else if(process.env.hasOwnProperty("COMPSOC")){ //running on OpenShift?
+    app.ipaddress = "0.0.0.0";
+    app.port      = "3000";
+} 
+else {
 	//otherwise assume running locally in test environment
 	app.ipaddress = "0.0.0.0";
 	app.port = "3000";
 }
 
 
-/*
-Define Middlewares
-*/
-
-//DON'T PLACE ANY APP.USE CALLS ABOVE COMPRESS.
-
 //send static data compressed with gzip.
-app.use(express.compress());
-
-//serve static files from the public directory (relative to the current directory by using the '__dirname' var)
-app.use('/public', express.static(__dirname + '/public', { maxAge: 0 }));
-
-//directory containing partials and views
-app.set('views', __dirname + '/views');
-
-//parses the post body of requests into request.body 
-app.use(express.bodyParser());
-
+//app.use(express.compress());                    
 //allows using PUT and DELETE along with GET and POST requests
-app.use(express.methodOverride());
-
+app.use(methodOverride());
+//set static folders
+app.use('/css', serveStatic(__dirname + '/public/css', { maxAge: 0 }));
+app.use('/js', serveStatic(__dirname + '/public/js', { maxAge: 0 }));
+app.use('/img', serveStatic(__dirname + '/public/img', { maxAge: 0 }));
+app.use('/fonts', serveStatic(__dirname + '/public/fonts', { maxAge: 0 }));
+app.use('/views', serveStatic(__dirname + '/public/views', { maxAge: 0 }));
 //allows defining of routes and routing variables
-app.use(app.router);
-
-
-app.use(function(req,res,next){
-    debug(req.method + ' ' + req.url);
-    next();
+app.use(router);
+//parses the post body of requests into request.body 
+app.use(bodyParser.json());
+//middleware to log every response for debugging
+app.use(function (req, res, next) {
+  console.log('\n New request:')
+  console.log('Time:', Date.now());
+  console.log('Path : '+req.path);
+  next();
 });
-/*
-* Define routes
-*/
 
+// Send the main layout which pulls in the other pages to be displayed
+app.get('/',function(req,res){
+    res.redirect('/views/main.html');
+});
 
-app.get('/', router.homepage);
-app.get('/partials/:name', router.partials);
-//api functions for all REST verbs
-app.get('/api/:controller/:method', router.api);
-app.put('/api/:controller/:method', router.api);
-app.post('/api/:controller/:method', router.api);
-app.delete('/api/:controller/:method', router.api);
-app.get('*', router.homepage);
-
-
-/*
-*	run server
-*/
-app.listen(app.port, app.ipaddress);
+// This route needs to be at the last defined to catch any 
+// other undefiend routes and redirect them home
+app.get('*',function(req,res){
+  console.log("Redirect to root")
+  res.redirect('/');
+});
+//run server
+app.listen(app.port,app.ipaddress); 
+console.log("server running @"+app.ipaddress+":"+app.port);
