c97f42e0345883ce6be60a812847f478b3872d0b
Liam Krewer <liam.krewer@gmail.com>
2014-10-13 22:19:51 +0100

    updated forever script

--- run_script.js
+++ run_script.js
@@ -1,6 +1,6 @@
 // 
 var forever = require('forever-monitor');
-
+var exec = require('child_process').exec;
 var child = new (forever.Monitor)('server.js', {
   max: 3,
   silent: true,
@@ -9,6 +9,16 @@ var child = new (forever.Monitor)('server.js', {
 
 child.on('exit', function () {
   console.log('server.js has exited after 3 restarts');
+  function puts(error, stdout, stderr) { sys.puts(stdout) }
+  exec("scripts/revert.sh", puts);
+});
+
+child.on('watch:restart', function(info) {
+    console.error('Restaring script because ' + info.file + ' changed');
+});
+
+child.on('restart', function() {
+    console.error('Forever restarting script for ' + child.times + ' time');
 });
 
 child.start();
